bb4b1cbbfc555f4fc849cfcdc10374ad36c54f19
Jay <91770194+jaymehta-g@users.noreply.github.com>
2024-01-30 17:29:16 -0600

    add signal to vision cmp

--- cmp/vision/vision.gd
+++ cmp/vision/vision.gd
@@ -1,15 +1,18 @@
 extends Area2D
 
 @export var detect_group: Groups.Names
+
+signal target_enterred()
+
 @onready var parent: Node2D = get_parent()
 
-func get_all_in_sight() -> Array[Node2D]:
+func get_all_targets() -> Array[Node2D]:
 	return get_overlapping_bodies() \
 		.filter(func(x): return x.is_in_group(Groups.group_name(detect_group)))
 
 # Nullable
-func get_closest() -> Node2D:
-	var all := get_all_in_sight()
+func get_closest_target() -> Node2D:
+	var all := get_all_targets()
 	if all.size() == 0:
 		return null
 	elif all.size() == 1:
@@ -17,7 +20,7 @@ func get_closest() -> Node2D:
 	var mypos := parent.position if parent else Vector2.ZERO
 	var best: Node2D = null
 	var bestdist_sq := 99999
-	for body: Node2D in get_all_in_sight():
+	for body: Node2D in all:
 		if body.position.distance_squared_to(mypos) < bestdist_sq:
 			best = body
 			bestdist_sq = body.position.distance_squared_to(mypos)
@@ -26,3 +29,6 @@ func get_closest() -> Node2D:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group(Groups.group_name(detect_group)): target_enterred.emit()
